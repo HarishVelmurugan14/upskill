78c739ee7090b7b6625afe1bb95c6d61aa37e4c9
harish.vk <harish.vk@zohocorp.com>
2026-05-04 18:57:36 +0530

    AQ2 binary search on array

--- Scaler/DSA/Advanced/Part2/d21_Searching_BinarySearchOnArray.java
+++ Scaler/DSA/Advanced/Part2/d21_Searching_BinarySearchOnArray.java
@@ -2,6 +2,8 @@ package DSA.Advanced.Part2;
 
 import Resources.Utilities.PrintHelper;
 
+import java.util.Arrays;
+
 /**
  * @author Harish Velmurugan
  * @last-modified 25-02-2025
@@ -26,7 +28,7 @@ public class d21_Searching_BinarySearchOnArray {
         d21SearchingBinarySearchOnArray.peakElement(); // Q4
 
         d21SearchingBinarySearchOnArray.searhAMatrix(); // AQ1
-        // AQ2
+        d21SearchingBinarySearchOnArray.minimumCostToBuildAnArray(3, 2,new int[][]{ {7, 3}, {2, 1},{4, 9} });// AQ2
         d21SearchingBinarySearchOnArray.maximumHeightOfAStaircase(20); // AQ3
 
 
@@ -289,6 +291,43 @@ public class d21_Searching_BinarySearchOnArray {
         }
         return steps;
     }
+
+    public int minimumCostToBuildAnArray(int A, int B, int[][] C) {
+      int x;
+      int ans = 1000000000; // Initialize answer as a large number
+      int lb;
+      for (int i = 0; i < A; i++) {
+         Arrays.sort(C[i]); // Sort each row of matrix
+      }
+      for (int i = 0; i < A - 1; i++) {
+         for (int j = 0; j < B; j++) {
+            lb = lower_bound(C[i + 1], C[i][j]); // check for next element as in soltion
+            if (lb != B) {
+               ans = Math.min(ans, Math.abs(C[i][j] - C[i + 1][lb])); // Update answer
+            }
+            if (lb != 0) {
+               ans = Math.min(ans, Math.abs(C[i][j] - C[i + 1][lb - 1]));
+            }
+         }
+      }
+      return ans;
+   }
+
+   // function used to find element index just greater than or equal to val
+   public int lower_bound(int a[], int val) {
+      int low = 0, high = a.length - 1, ans = a.length;
+      while (low <= high) {
+         int mid = (high - low) / 2 + low;
+         if (a[mid] < val) {
+            low = mid + 1;
+         } else {
+            ans = mid;
+            high = mid - 1;
+         }
+      }
+      return ans;
+   }
+
 
     /* Section : ------------------------------- [ Problems ] ------------------------------- */
 
